7e55462a5b5dd49a617976805a1008897aab0cf8
Dustin Dinh <djdinh@gmail.com>
2015-04-23 16:33:06 -0700

    Finished favorites. Started at 12pm

--- ass4.js
+++ ass4.js
@@ -1,6 +1,9 @@
 
 var gistPulls = null;
 var localGists = null;
+var localFavorites = null;
+
+var favoriteGists = null;
 
 window.onload = function()
 {
@@ -36,8 +39,14 @@ function pullGithub()
     var gitURL5 = 'https://api.github.com/gists?page=5'
 
     httpRequest.onreadystatechange = alertContents;
-    httpRequest.open('GET', gitURL2);
+    httpRequest.open('GET', gitURL);
     httpRequest.send();
+    saveGists();
+    // httpRequest.onreadystatechange = alertContents;
+    // httpRequest.open('GET', gitURL);
+    // saveGists()
+    // httpRequest.send();
+
 //   }
 
   function alertContents() 
@@ -64,14 +73,10 @@ function pullGithub()
     console.log('Saved to gists to local!');
   }
 
+
   function printLanguage()
   {
-    localGists = localStorage.getItem('savedGists');
-    if (gistPulls == null)
-    {
-      console.log('gistPulls is null, pulling from local');
-      gistPulls = JSON.parse(localGists);
-    }
+
 
  
     console.log(gistPulls.length);
@@ -104,34 +109,134 @@ function liGists(gists)
     var entry = dlEntry('Description', gistPulls[index].description);
     dl.appendChild(entry.dt);
     dl.appendChild(entry.dd);
-    index++;
+    
     return dl;
   }
 
-  function createGistsList(ul)
+function createGistsList(ul)
   {
+    index = 0;
+
     gistPulls.forEach(function(s) 
     {
+      var favorite = document.createElement("button");
+      favorite.setAttribute('id', index); 
+      favorite.setAttribute("value", index)
+      favorite.setAttribute("onclick", 'saveToFavorites(this)');
       var li = document.createElement('li');
       li.appendChild(liGists(s));
+      li.appendChild(favorite);
+      favorite.innerHTML = favorite.innerHTML + 'Add to favorites';
       ul.appendChild(li);
 
+      index++;
+
     });
+
+
   }
 
-  function displayGists()
+function displayGists()
+{
+  document.getElementById('gistsList').innerHTML = "";
+  if (gistPulls == null)
   {
-    if (gistPulls == null)
-    {
-      gistPulls = {'gists':[]};
-      console.log('success!');
-    }
-
-    createGistsList(document.getElementById('gistsList'));
+    gistPulls = {'gists':[]};
+    console.log('success!');
   }
+
+  createGistsList(document.getElementById('gistsList'));
+}
 
 function test()
 {
   console.log('great success!');
   console.log(document.getElementById('displayPages').value)
 }
+
+function saveToFavorites(what)
+{
+  favoriteGists.push(what.value);
+  localStorage.setItem('favoriteGists', JSON.stringify(favoriteGists));
+}
+
+function printFavorites()
+{
+
+  document.getElementById('gistsList').innerHTML = "";
+  
+  for (var i = 0; i < favoriteGists.length; i++)
+  {
+
+    console.log(favoriteGists[i]);
+  }
+
+
+  if (gistPulls == null)
+  {
+    gistPulls = {'gists':[]};
+    console.log('success!');
+  }
+
+  favoritesCreateGistsList(document.getElementById('gistsList'));
+  
+}
+
+window.onload = function(){
+  localGists = localStorage.getItem('savedGists');
+  if (gistPulls == null)
+    {
+      console.log('gistPulls is null, pulling from local');
+      gistPulls = JSON.parse(localGists);
+    }
+
+  localFavorites = localStorage.getItem('favoriteGists');
+  if (favoriteGists == null)
+  {
+    console.log('favorites gists is null pulling from local');
+    favoriteGists = JSON.parse(localFavorites);
+  }
+
+
+}
+
+function favoritesDlEntry(term, definition) 
+{
+  var dt = document.createElement('dt');
+  var dd = document.createElement('dd');
+
+  dt.innerText = term;
+  dd.innerText = definition;
+  return {'dt':dt, 'dd':dd};
+}
+//index for the gistPulls because I'm not sure how to keep track of items in array
+var index = 0;
+
+function favoritesLiGists(gists)
+  {
+    var dl = document.createElement('dl');
+    var entry = dlEntry('URL', gistPulls[favoriteGists[index]].url);
+    dl.appendChild(entry.dt);
+    dl.appendChild(entry.dd);
+    var entry = dlEntry('Description', gistPulls[favoriteGists[index]].description);
+    dl.appendChild(entry.dt);
+    dl.appendChild(entry.dd);
+    
+    return dl;
+  }
+
+function favoritesCreateGistsList(ul)
+  {
+    index = 0;
+
+    for (var i = 0; i < favoriteGists.length; i++)
+    {
+      var li = document.createElement('li');
+      li.appendChild(favoritesLiGists(favoriteGists[i]));
+      ul.appendChild(li);
+
+      index++;
+    };
+
+
+  }
